47bef3f5c0d333e0d78c353dee12541c3f95ec33
Lucien Greathouse <me@lpghatguy.com>
2020-02-05 14:46:45 -0800

    Update rbx_dom_test

--- rbx_dom_test/src/lib.rs
+++ rbx_dom_test/src/lib.rs
@@ -1,5 +1,3 @@
-mod builder;
 mod viewer;
 
-pub use builder::*;
 pub use viewer::*;
--- rbx_dom_test/src/viewer.rs
+++ rbx_dom_test/src/viewer.rs
@@ -1,12 +1,16 @@
 use std::collections::{BTreeMap, HashMap};
 
-use rbx_dom_weak::{RbxId, RbxTree, RbxValue};
+use rbx_dom_weak::{
+    types::{Ref, Variant},
+    WeakDom,
+};
+
 use serde::{Deserialize, Serialize};
 
-/// Contains state for viewing/redacting RbxTree objects, making them suitable
+/// Contains state for viewing/redacting WeakDom objects, making them suitable
 /// for viewing in a snapshot test.
 pub struct TreeViewer {
-    id_map: HashMap<RbxId, String>,
+    id_map: HashMap<Ref, String>,
     next_id: usize,
 }
 
@@ -18,16 +22,15 @@ impl TreeViewer {
         }
     }
 
-    pub fn view(&mut self, tree: &RbxTree) -> ViewedInstance {
-        let root_id = tree.get_root_id();
+    pub fn view(&mut self, tree: &WeakDom) -> ViewedInstance {
+        let root_id = tree.root_ref();
         self.populate_id_map(tree, root_id);
         self.view_instance(tree, root_id)
     }
 
-    pub fn view_children(&mut self, tree: &RbxTree) -> Vec<ViewedInstance> {
-        let root_id = tree.get_root_id();
-        let root_instance = tree.get_instance(root_id).unwrap();
-        let children = root_instance.get_children_ids();
+    pub fn view_children(&mut self, tree: &WeakDom) -> Vec<ViewedInstance> {
+        let root_instance = tree.root();
+        let children = root_instance.children();
 
         for &id in children {
             self.populate_id_map(tree, id);
@@ -39,21 +42,21 @@ impl TreeViewer {
             .collect()
     }
 
-    fn populate_id_map(&mut self, tree: &RbxTree, id: RbxId) {
+    fn populate_id_map(&mut self, tree: &WeakDom, id: Ref) {
         self.id_map.insert(id, format!("id-{}", self.next_id));
         self.next_id += 1;
 
-        let instance = tree.get_instance(id).unwrap();
-        for id in instance.get_children_ids() {
+        let instance = tree.get_by_ref(id).unwrap();
+        for id in instance.children() {
             self.populate_id_map(tree, *id);
         }
     }
 
-    fn view_instance(&self, tree: &RbxTree, id: RbxId) -> ViewedInstance {
-        let instance = tree.get_instance(id).unwrap();
+    fn view_instance(&self, tree: &WeakDom, id: Ref) -> ViewedInstance {
+        let instance = tree.get_by_ref(id).unwrap();
 
         let children = instance
-            .get_children_ids()
+            .children()
             .iter()
             .copied()
             .map(|id| self.view_instance(tree, id))
@@ -65,12 +68,10 @@ impl TreeViewer {
             .map(|(key, value)| {
                 let key = key.clone();
                 let new_value = match value {
-                    RbxValue::Ref {
-                        value: Some(ref_id),
-                    } => {
+                    Variant::Ref(ref_id) => {
                         let id_str = self
                             .id_map
-                            .get(ref_id)
+                            .get(&ref_id)
                             .cloned()
                             .unwrap_or_else(|| "[unknown ID]".to_owned());
                         ViewedValue::Ref(id_str)
@@ -85,14 +86,14 @@ impl TreeViewer {
         ViewedInstance {
             id: self.id_map.get(&id).unwrap().clone(),
             name: instance.name.clone(),
-            class_name: instance.class_name.clone(),
+            class_name: instance.class.clone(),
             properties,
             children,
         }
     }
 }
 
-/// A transformed view into an RbxTree or RbxInstance that has been redacted and
+/// A transformed view into an WeakDom or RbxInstance that has been redacted and
 /// transformed to be more readable.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ViewedInstance {
@@ -103,51 +104,38 @@ pub struct ViewedInstance {
     children: Vec<ViewedInstance>,
 }
 
-/// Wrapper around RbxValue with refs replaced to be redacted, stable versions
+/// Wrapper around Variant with refs replaced to be redacted, stable versions
 /// of their original IDs.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum ViewedValue {
     Ref(String),
-    Other(RbxValue),
+    Other(Variant),
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    use rbx_dom_weak::RbxInstanceProperties;
+    use rbx_dom_weak::InstanceBuilder;
 
     #[test]
     fn redact_single() {
-        let tree = RbxTree::new(RbxInstanceProperties {
-            name: "Root".to_owned(),
-            class_name: "Folder".to_owned(),
-            properties: HashMap::new(),
-        });
+        let tree = WeakDom::new(InstanceBuilder::new("Folder").with_name("Root"));
 
         insta::assert_yaml_snapshot!(TreeViewer::new().view(&tree));
     }
 
     #[test]
     fn redact_multi() {
-        let mut tree = RbxTree::new(RbxInstanceProperties {
-            name: "Root".to_owned(),
-            class_name: "Folder".to_owned(),
-            properties: HashMap::new(),
-        });
+        let mut tree = WeakDom::new(InstanceBuilder::new("Folder").with_name("Root"));
 
-        let root_id = tree.get_root_id();
+        let root_id = tree.root_ref();
 
         for i in 0..4 {
-            let name = format!("Child {}", i);
-            let properties = RbxInstanceProperties {
-                name,
-                class_name: "Folder".to_owned(),
-                properties: HashMap::new(),
-            };
+            let builder = InstanceBuilder::new("Folder").with_name(format!("Child {}", i));
 
-            tree.insert_instance(properties, root_id);
+            tree.insert(root_id, builder);
         }
 
         insta::assert_yaml_snapshot!(TreeViewer::new().view(&tree));
@@ -155,21 +143,13 @@ mod test {
 
     #[test]
     fn redact_values() {
-        let mut tree = RbxTree::new(RbxInstanceProperties {
-            name: "Root".to_owned(),
-            class_name: "ObjectValue".to_owned(),
-            properties: HashMap::new(),
-        });
+        let mut tree = WeakDom::new(InstanceBuilder::new("ObjectValue").with_name("Root"));
 
-        let root_id = tree.get_root_id();
-        let root_instance = tree.get_instance_mut(root_id).unwrap();
+        let root_instance = tree.root_mut();
 
-        root_instance.properties.insert(
-            "Value".to_owned(),
-            RbxValue::Ref {
-                value: Some(root_id),
-            },
-        );
+        root_instance
+            .properties
+            .insert("Value".to_owned(), Variant::Ref(root_instance.referent()));
 
         insta::assert_yaml_snapshot!(TreeViewer::new().view(&tree));
     }
